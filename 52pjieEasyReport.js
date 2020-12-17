7788d9385c91c99f925ad871793ed82aa08595f8
sanshuifeibing <sanshuifeibing@qq.com>
2020-12-17 22:03:04 +0800

    update

--- 52pjieEasyReport.js
+++ 52pjieEasyReport.js
@@ -14,6 +14,7 @@
     var btnLogout = getNodeByXpath("//*[@id=\"um\"]/p[1]/a[6]");
     var reportUrl = "https://www.52pojie.cn/forum.php?mod=post&action=newthread&fid=15";
     if (window.location.href != reportUrl) {
+
         if (btnLogin == null && btnLogout != null) {
             var webHtmls = document.getElementById("postlist").getElementsByTagName("div")[2].getElementsByClassName("plhin res-postfirst")[0];
             if (webHtmls != null) {
@@ -21,10 +22,13 @@
                 var x = getNodeByXpath("//*[@id=\"" + pid + "\"]/tbody/tr[4]/td[2]/div/div/p");
                 var btnReportHtml = "<a href=\"" + reportUrl + "\" style=\"color:red;\">高级举报</a>";
                 x.innerHTML += btnReportHtml;
+                var btnScore = getNodeByXpath("//*[@id=\"ratelog_" + pid.replace("pid", "") + "\"]/dd/table/tbody[1]/tr/th[4]/a");
+                if (btnScore.innerText == "收起") {
+                    btnScore.click();
+                }
                 var author = getNodeByXpath("//*[@id=\"favatar" + pid.replace("pid", "") + "\"]/div[1]/div/a");
                 var createTime = getNodeByXpath("//*[@id=\"authorposton" + pid.replace("pid", "") + "\"]");
-                var patt = new RegExp("(?<=发表于</span> ).*");
-                var postsInfo = { Author: author.innerHTML, CreateTime: patt.exec(createTime.innerHTML)[0] };
+                var postsInfo = { Author: author.innerHTML, CreateTime:createTime.innerText.trim().replace("发表于","") };
                 GM_setValue('info', postsInfo);
             }
         }
@@ -34,7 +38,7 @@
         getNodeByXpath("//*[@id=\"typeoption_Complaints_Evidence\"]").value = document.referrer;
         getNodeByXpath("//*[@id=\"typeoption_Complaints_ID\"]").value = GM_getValue('info').Author;
         getNodeByXpath("//*[@id=\"typeoption_Complaints_Time\"]").value = GM_getValue('info').CreateTime;
-
+        //this.hideMenu('fwin_dialog', 'dialog');  //取消此行注释即开启屏蔽版规提醒，注意这是给熟悉版规的同学提供的哦，不熟悉的请乖乖看完版规，不然会被黑米拉进小黑屋@#￥！……&#%哦~
         console.log("举报材料：");
         console.log("帖子地址：" + document.referrer);
         console.log("论坛账号：" + GM_getValue('info').Author);
@@ -45,6 +49,3 @@
         return result.iterateNext()
     }
 })();
-
-
-
