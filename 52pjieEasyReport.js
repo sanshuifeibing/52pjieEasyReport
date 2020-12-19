5eb59a9e759a607a67563518f09559bf97436a01
sanshuifeibing <sanshuifeibing@qq.com>
2020-12-19 19:13:39 +0800

    update v0.2.5

--- 52pjieEasyReport.js
+++ 52pjieEasyReport.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         吾爱破解论坛快捷举报
 // @namespace    https://www.sanshuifeibing.com/
-// @version      0.2.1
+// @version      0.2.5
 // @description  对违法乱纪的帖子进行快捷举报，营造良好的技术学习氛围。
 // @author       三水非冰
 // @match        https://www.52pojie.cn/*
@@ -13,35 +13,35 @@
 (function () {
     let userName = $(".vwmy").children("a").text();
     let isHasPosts = getPostsStatus(userName);
-    let btnLogin = getNodeByXpath("//*[@id=\"lsform\"]/div/div[1]/table/tbody/tr[2]/td[3]/button/em");
-    let btnLogout = getNodeByXpath("//*[@id=\"um\"]/p[1]/a[6]");
+    let btnLoginText = $("button.pn.vm").find("em").text().trim();
+    let btnLogoutText = $("#um").find("a").eq(7).text().trim();
     let reportUrl = "https://www.52pojie.cn/forum.php?mod=post&action=newthread&fid=15";
     if (isHasPosts) {
         reportUrl = getPostsUrl(userName);
     }
     if (window.location.href != reportUrl) {
-        if (btnLogin == null && btnLogout != null) {
+        if (btnLoginText != "登录" && btnLogoutText == "退出") {
             let pid = $("#postlist div").eq(2).attr("id").replace("post_", "");
             let x = $("#pid" + pid).find("p:contains('举报')");
             let btnReportHtml = "<a href=\"javascript:;\" style=\"color:red;\" id=\"btnReport\">高级举报</a>";
-            if ($("a.xw1").eq(0).text().trim() != userName) {
+            if ($("a.xw1").eq(0).text().trim() != userName && $("#postnum" + pid).find("b").text().trim() == "楼主") {
                 x.append(btnReportHtml);
             }
             if (/收起/.test($("#ratelog_" + pid).find("th").text())) {
                 $("#ratelog_" + pid).find("th:contains('收起')").children("a").click();
             }
-            let author = getNodeByXpath("//*[@id=\"favatar" + pid + "\"]/div[1]/div/a");
-            let createTime = getNodeByXpath("//*[@id=\"authorposton" + pid + "\"]");
-            let postsInfo = { Author: author.innerHTML, CreateTime: createTime.innerText.replace("发表于", "").trim() };
+            let author = $("#favatar" + pid + ".pls.favatar").find("a.xw1").text().trim();
+            let createTime = $("#authorposton" + pid).text().replace("发表于", "").trim();
+            let postsInfo = { Author: author, CreateTime: createTime };
             GM_setValue('info', postsInfo);
         }
     }
     else {
         if (isHasPosts == false) {
-            getNodeByXpath("//*[@id=\"typeid_ctrl_menu\"]/ul/li[3]").click();
-            getNodeByXpath("//*[@id=\"typeoption_Complaints_Evidence\"]").value = document.referrer;
-            getNodeByXpath("//*[@id=\"typeoption_Complaints_ID\"]").value = GM_getValue('info').Author;
-            getNodeByXpath("//*[@id=\"typeoption_Complaints_Time\"]").value = GM_getValue('info').CreateTime;
+            $("#typeid_ctrl_menu").find("li").nextAll().eq(1).click();
+            $("#typeoption_Complaints_ID").val(GM_getValue('info').Author);
+            $("#typeoption_Complaints_Evidence").val(document.referrer);
+            $("#typeoption_Complaints_Time").val(GM_getValue('info').CreateTime);
             //this.hideMenu('fwin_dialog', 'dialog');  //取消此行注释即开启屏蔽版规提醒，注意这是给熟悉版规的同学提供的哦，不熟悉的请乖乖看完版规，不然会被黑米拉进小黑屋@#￥！……&#%哦~
         }
         else {
@@ -58,10 +58,6 @@
     $("#btnReport").click(function () {
         window.location.href = reportUrl;
     });
-    function getNodeByXpath(xpath) {
-        let result = document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null);
-        return result.iterateNext()
-    }
     function getPostsStatus(userName) {
         let ret = false;
         $.ajax({
